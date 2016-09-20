468f7fa7b6b9d78857a6e42c80ec71cb2b9671d1
Damien Garaud <damien.garaud@gmail.com>
2016-09-20 23:35:48 +0200

    try CMC predictions with processed categorical data

--- cmc_random_forest.py
+++ cmc_random_forest.py
@@ -18,14 +18,10 @@ COLNAMES = ['age', 'education', 'husband_education', 'children', 'religion',
             'working', 'husband_occupation', 'living_index', 'media', 'method']
 
 
-if __name__ == '__main__':
-    Logger.info("read input file '%s'", FNAME)
-    data = pd.read_csv(FNAME, names=COLNAMES, header=None)
+def main(data):
     X, y = data.drop('method', axis=1).values, data['method'].values
-
     Logger.info("split train/test data")
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=6)
-
     Logger.info("learn")
     clf = RandomForestClassifier(n_estimators=100)
     score = cross_val_score(clf, X_train, y_train, cv=5)
@@ -33,3 +29,24 @@ if __name__ == '__main__':
     clf.fit(X_train, y_train)
     print("train mean score: %0.6f (+/- %0.3f)" % (score.mean(), score.std()))
     print("test score: %0.6f" % clf.score(X_test, y_test))
+    return clf
+
+
+if __name__ == '__main__':
+    Logger.info("read input file '%s'", FNAME)
+    data = pd.read_csv(FNAME, names=COLNAMES, header=None)
+    # X, y = data.drop('method', axis=1).values, data['method'].values
+
+    print("raw data")
+    clf1 = main(data)
+
+    df_2 = data.drop(['education', 'husband_education',
+                      'husband_occupation', 'living_index'], axis=1)
+    dummy = lambda x: pd.get_dummies(data[x], prefix=x, drop_first=True)
+    df_2 = pd.concat([df_2, dummy('education'),
+                      dummy('husband_education'),
+                      dummy('husband_occupation'),
+                      dummy('living_index')], axis=1)
+
+    print("turn dummies data")
+    clf2 = main(df_2)
--- cmc_xgboost.py
+++ cmc_xgboost.py
@@ -17,14 +17,10 @@ COLNAMES = ['age', 'education', 'husband_education', 'children', 'religion',
             'working', 'husband_occupation', 'living_index', 'media', 'method']
 
 
-if __name__ == '__main__':
-    Logger.info("read input file '%s'", FNAME)
-    data = pd.read_csv(FNAME, names=COLNAMES, header=None)
+def main(data):
     X, y = data.drop('method', axis=1).values, data['method'].values
-
     Logger.info("split train/test data")
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=6)
-
     Logger.info("learn")
     # clf = xgb.XGBClassifier(n_estimators=200, learning_rate=0.05,
     #                         objective='multi:softmax', max_depth=5,
@@ -37,3 +33,24 @@ if __name__ == '__main__':
     clf.fit(X_train, y_train)
     print("train mean score: %0.6f (+/- %0.3f)" % (score.mean(), score.std()))
     print("test score: %0.6f" % clf.score(X_test, y_test))
+    return clf
+
+
+if __name__ == '__main__':
+    Logger.info("read input file '%s'", FNAME)
+    data = pd.read_csv(FNAME, names=COLNAMES, header=None)
+
+
+    print("raw data")
+    clf1 = main(data)
+
+    df_2 = data.drop(['education', 'husband_education',
+                      'husband_occupation', 'living_index'], axis=1)
+    dummy = lambda x: pd.get_dummies(data[x], prefix=x, drop_first=True)
+    df_2 = pd.concat([df_2, dummy('education'),
+                      dummy('husband_education'),
+                      dummy('husband_occupation'),
+                      dummy('living_index')], axis=1)
+
+    print("turn dummies data")
+    clf2 = main(df_2)
